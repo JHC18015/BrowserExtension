23f49a5b21ca36f1c4352e08c6f9f93e5ea15e63
JHC18015 <162929199+JHC18015@users.noreply.github.com>
2024-03-11 15:00:14 +1300

    Update content.js

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ console.log("The extension is working!")
 
 var imgs = document.getElementsByTagName("img");
 var imgSrcs = [];
-console.log(chrome.runtime.getURL("sexybeast.jpg"));    
+console.log(chrome.runtime.getURL("sexybeast.JPG"));    
 for (var i = 0; i < imgs.length; i++) {
-    imgs[i].src = chrome.runtime.getURL("sexybeast.jpg");
+    imgs[i].src = chrome.runtime.getURL("sexybeast.JPG");
 }
