25d0141776a6f03dce16c00067e5fb22760048df
JHC18015 <162929199+JHC18015@users.noreply.github.com>
2024-03-11 14:44:38 +1300

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,8 @@
-alert("Jimmy has commenced the toe sucking!")
+console.log("The extension is working!")
 
 var imgs = document.getElementsByTagName("img");
 var imgSrcs = [];
-    
+console.log(chrome.runtime.getURL("sexybeast.jpg"));    
 for (var i = 0; i < imgs.length; i++) {
     imgs[i].src = chrome.runtime.getURL("sexybeast.jpg");
 }
